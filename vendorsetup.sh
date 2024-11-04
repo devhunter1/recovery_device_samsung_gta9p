4b25564b60879d9740fd9a8a7c6d5dc73f3fda45
physwizz <pollockg86@gmail.com>
2024-11-05 09:21:25 +1100

    fix vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_gta9pwifi-user
-add_lunch_combo omni_gta9pwifi-userdebug
-add_lunch_combo omni_gta9pwifi-eng
+add_lunch_combo twrp_gta9pwifi-user
+add_lunch_combo twrp_gta9pwifi-userdebug
+add_lunch_combo twrp_gta9pwifi-eng
